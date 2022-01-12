01814cb646859d773761dcf80a7251362170c60d
Ifeanyi Osi-Okeke <ifeanyi.osi.okeke@gmail.com>
2022-01-12 18:42:42 +0100

    FAQ page

---
+++ index.js
@@ -0,0 +1,18 @@
+
+let faq = document.getElementsByClassName("faq-page");
+let i;
+
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        
+        this.classList.toggle("active");
+
+        
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
